d9f5e274f7522289ea4b4e2b35897e06c86ee416
Glenn Chang <glchang@bcgsc.ca>
2022-12-15 14:48:35 -0800

    Changed theme of figures and turn pie chart to bar chart

--- scripts/cancerFigures.R
+++ scripts/cancerFigures.R
@@ -1,5 +1,6 @@
 suppressMessages(library(tidyverse))
 suppressMessages(library(optparse))
+suppressMessages(library("ggsci"))
 
 
 # Make help options
@@ -37,7 +38,16 @@ if ("cnv_state" %in% column) {
     ggtitle(paste0("Gene frequency in each copy number variant state"), 
             subtitle = sample) +
     ylab("Gene Count") + 
-    xlab("Copy number variant state") 
+    xlab("Copy number variant state") + 
+    theme_bw() + 
+    scale_color_npg() + 
+    scale_fill_npg() + 
+    theme(axis.line = element_line(colour = "black"),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          panel.background = element_blank()) 
+  
   
   ggsave(filename = paste0(out, "/cnvBar.pdf"), plot = cnvBar, units = "in")
   
@@ -50,7 +60,17 @@ if ("cnv_state" %in% column) {
     geom_vline(xintercept = 0.10) +
     geom_hline(yintercept = 0.05) +
     xlab("MAF - Expectd MAF from CNV") + 
-    ylab("Adjusted pvalue")
+    ylab("Adjusted pvalue") + 
+    ggtitle(paste0("Copy Number Variants adjusted for expected MAF"), 
+            subtitle = sample) +
+    theme_bw() + 
+    scale_fill_npg() +
+    scale_color_npg() + 
+    theme(axis.line = element_line(colour = "black"),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          panel.background = element_blank()) 
   
   ggsave(filename = paste0(out, "/expectMAF.pdf"), plot = expectMAF, units = "in")
   
@@ -68,8 +88,8 @@ if ("methyl_state" %in% column) {
     dplyr::filter(!is.na(methyl_state)) %>%
     dplyr::filter(aseResults == "ASE") %>%
     dplyr::mutate(methylation = case_when(
-      methyl_state < 0 ~ "allele1Methyl",
-      TRUE ~ "allele2Methyl"
+      methyl_state < 0 ~ "allele2Methyl",
+      TRUE ~ "allele1Methyl"
     )) %>%
     dplyr::mutate(expression = case_when(
       allele1IsMajor ~ "alelle1Expression",
@@ -77,14 +97,24 @@ if ("methyl_state" %in% column) {
     )) %>%
     dplyr::select(methylation, expression) %>%
     group_by(methylation, expression) %>%
-    summarize(n=n()) 
+    summarize(n=n())
     
   
   dmr_contigency <- ggplot(data =  dmr, mapping = aes(x = methylation, y = expression)) +
     geom_tile(aes(fill = n), colour = "white") +
     geom_text(aes(label = sprintf("%1.0f", n)), vjust = 1, colour = "white") +
     scale_fill_gradient(low = "blue", high = "red") +
-    theme_bw() + theme(legend.position = "none")
+    ggtitle("Allelic Methylation and Expression Contigency Table", subtitle = sample) +
+    theme_bw() + 
+    theme(legend.position = "none", 
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(), 
+          panel.border = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_blank(), 
+          axis.title.x=element_blank(), 
+          axis.title.y=element_blank())  
+  
   ggsave(filename = paste0(out, "/dmrContingency.pdf"), plot = dmr_contigency, units = "in")
   
   dmr %>%
@@ -126,8 +156,16 @@ if ("stop_variant_allele" %in% column) {
       geom_tile(aes(fill = n), colour = "white") +
       geom_text(aes(label = sprintf("%1.0f", n)), vjust = 1, colour = "white") +
       scale_fill_gradient(low = "blue", high = "red") +
-      theme_bw() + theme(legend.position = "none") +
-      ggtitle("ASE vs Stop gain/loss contingency table", subtitle = sample)
+      theme_bw() +
+      ggtitle("ASE vs Stop gain/loss contingency table", subtitle = sample) + 
+      theme(legend.position = "none", 
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(), 
+            panel.border = element_blank(),
+            axis.ticks.x = element_blank(),
+            axis.ticks.y = element_blank(), 
+            axis.title.x=element_blank(), 
+            axis.title.y=element_blank()) 
   ggsave(filename = paste0(out, "/stopVarContingency.pdf"), plot = stopContingency, units = "in")
   
   stopVarTable <- summary_table %>%
@@ -144,11 +182,20 @@ if ("stop_variant_allele" %in% column) {
   stopBar <-  stopVarTable %>%
     dplyr::filter(stopVar) %>%
     ggplot(aes(aseResults, percent)) +
-    geom_bar(stat = "identity") +
+    geom_bar(aes(fill = aseResults ), stat = "identity") +
     ylab("Proportion of genes") +
     coord_flip() +
     ggtitle("Proportion of genes with stop gain or loss variant", 
-            subtitle = sample)
+            subtitle = sample) + 
+    theme_bw() + 
+    scale_fill_npg() +
+    scale_color_npg() + 
+    theme(axis.line = element_line(colour = "black"),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          panel.background = element_blank(), 
+          legend.position = "none") 
   ggsave(filename = paste0(out, "/stopVarBar.pdf"), plot = stopBar, units = "in")
 }
   
@@ -174,11 +221,20 @@ if ("somaticSNV" %in% column) {
   snv <- snvTable %>%
     dplyr::filter(somaticSNV) %>%
     ggplot(aes(aseResults, percent)) +
-    geom_bar(stat = "identity") +
+    geom_bar(aes(fill = aseResults), stat = "identity") +
     ylab("Proportion of genes") +
     coord_flip() +
     ggtitle("Proportion of genes with somatic SNV mutation", 
-            subtitle = sample)
+            subtitle = sample) + 
+    theme_bw() + 
+    scale_fill_npg() +
+    scale_color_npg() + 
+    theme(axis.line = element_line(colour = "black"),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          panel.background = element_blank(), 
+          legend.position = "none") 
   ggsave(filename = paste0(out, "/somaticSNVbar.pdf"), plot = snv, units = "in")
 }
 
@@ -203,11 +259,20 @@ if ("somaticIndel" %in% column) {
   indel <- indelTable %>%
     dplyr::filter(somaticIndel) %>%
     ggplot(aes(aseResults, percent)) +
-    geom_bar(stat = "identity") +
+    geom_bar(aes(fill = aseResults), stat = "identity") +
     ylab("Proportion of genes") +
     coord_flip() +
     ggtitle("Proportion of genes with somatic indel mutation", 
-            subtitle = sample)
+            subtitle = sample) + 
+    theme_bw() + 
+    scale_fill_npg() +
+    scale_color_npg() + 
+    theme(axis.line = element_line(colour = "black"),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          panel.background = element_blank(), 
+          legend.position = "none") 
   ggsave(filename = paste0(out, "/somaticIndelBar.pdf"), plot = indel, units = "in")
 }
 
@@ -231,88 +296,23 @@ if ("tf_allele" %in% column) {
   tfbs <- tfbsTable %>%
     dplyr::filter(tfbsVar) %>%
     ggplot(aes(aseResults, percent)) +
-    geom_bar(stat = "identity")+
+    geom_bar(aes(fill = aseResults), stat = "identity")+
     ylab("Proportion of genes") +
     coord_flip() +
-    ggtitle("Proportion of genes with mutation in transcription factor binding site", 
-            subtitle = sample)
+    ggtitle("Proportion of genes with mutation in \ntranscription factor binding site", 
+            subtitle = sample) + 
+    theme_bw() + 
+    scale_fill_npg() +
+    scale_color_npg() + 
+    theme(axis.line = element_line(colour = "black"),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          panel.background = element_blank(), 
+          legend.position = "none") 
   ggsave(filename = paste0(out, "/tfbsBar.pdf"), plot = tfbs, units = "in")
 }
 
-
-#######################
-# Explanation Pie Chart
-#######################
-
-summaryTableExplain <- data.frame(summary_table) %>%
-  dplyr::filter(aseResults == "ASE") %>%
-  dplyr::mutate(explanation = NA)
-
-if ("cnv_state" %in% column) {
-  summaryTableExplain$cnvRatioDiff = summaryTableExplain$majorAlleleFrequency - summaryTableExplain$expectedMAF
-  summaryTableExplain$explanation[is.na(summaryTableExplain$explanation) & 
-                                summaryTableExplain$cnv_state=="LOH"] <- "LOH"
-  summaryTableExplain$explanation[is.na(summaryTableExplain$explanation) & 
-                                summaryTableExplain$cnvRatioDiff< 0.10] <- "CNV"
-}
-
-if ("somaticSNV" %in% column) {
-  summaryTableExplain$explanation[is.na(summaryTableExplain$explanation) & 
-                                summaryTableExplain$somaticSNV] <- "Somatic Mutation"
-}
-if ("somaticIndel" %in% column) {
-  summaryTableExplain$explanation[is.na(summaryTableExplain$explanation) & 
-                                summaryTableExplain$somaticIndel] <- "Somatic Mutation"
-}
-
-if ("methyl_state" %in% column) {
-  summaryTableExplain$explanation[is.na(summaryTableExplain$explanation) & 
-                                summaryTableExplain$allele1IsMajor & 
-                                !is.na(summaryTableExplain$methyl_state) & 
-                                summaryTableExplain$methyl_state < 0] <- "Alleleic Methylation"
-  summaryTableExplain$explanation[is.na(summaryTableExplain$explanation) & 
-                                !summaryTableExplain$allele1IsMajor & 
-                                !is.na(summaryTableExplain$methyl_state) & 
-                                summaryTableExplain$methyl_state > 0] <- "Alleleic Methylation"
-}
-
-if ("stop_variant_allele" %in% column) {
-  summaryTableExplain$explanation[is.na(summaryTableExplain$explanation) & 
-                                summaryTableExplain$allele1IsMajor & 
-                                summaryTableExplain$stop_variant_allele == 2] <- "Stop gain/loss mutation"
-  summaryTableExplain$explanation[is.na(summaryTableExplain$explanation) & 
-                                !summaryTableExplain$allele1IsMajor & 
-                                summaryTableExplain$stop_variant_allele == 1] <- "Stop gain/loss mutation"
-}
-if ("tf_allele" %in% column) {
-  summaryTableExplain$explanation[is.na(summaryTableExplain$explanation) & 
-                                !is.na(summaryTableExplain$transcriptionFactor)] <- "TFBS mutation"
-}
-
-
-summaryTableExplain$explanation[is.na(summaryTableExplain$explanation)] <- "Unknown"
-
-summaryTableExplain %>%
-  dplyr::select(gene, cancer_gene, explanation) %>%
-  dplyr::mutate(sample = sample) %>%
-  write.table(file = paste0(out, "/tables/aseCause.tsv"),
-              quote = F, sep = "\t", row.names = F, col.names = T)
-
-
-asePie <- summaryTableExplain %>%
-  group_by(explanation) %>%
-  summarize(n=n()) %>%
-  ggplot(aes(x="", y=n, fill=explanation)) +
-  geom_bar(width = 1, stat = "identity") + 
-  coord_polar("y", start=0) +
-  ggtitle("Causes of allelic expression", subtitle = sample) +
-  theme(axis.title.x=element_blank(),
-        axis.title.y=element_blank(),
-        axis.text.y=element_blank(),
-        axis.ticks = element_blank()) + 
-  labs(fill='ASE Causes') 
-
-ggsave(filename = paste0(out, "/aseCause.pdf"), plot = asePie, units = "in")
 
 #######################
 # Cancer Figures
@@ -325,19 +325,142 @@ cancer_bar <- summary_table %>%
 ggsave(filename = paste0(out, "/cancerBar.pdf"), plot = cancer_bar, units = "in")
 
 #######################
-# Cancer Explain Figures
+# Mechanism function
 #######################
-cancerCause <- summaryTableExplain %>%
-  dplyr::filter(cancer_gene) %>%
-  group_by(explanation) %>%
-  summarize(n=n()) %>%
-  ggplot(aes(x="", y=n, fill=explanation)) +
-  geom_bar(width = 1, stat = "identity") + 
-  coord_polar("y", start=0) +
-  ggtitle("Cause of allelic expression in cancer genes", subtitle = sample) +
-  theme(axis.title.x=element_blank(),
-        axis.title.y=element_blank(),
-        axis.text.y=element_blank(),
-        axis.ticks = element_blank())
 
+causeFunction <- function(dataset) {
+  summaryTableExplain <- dataset %>%
+    dplyr::filter(aseResults == "ASE") 
+  ASEunknown <- summaryTableExplain
+  cause_final <- tibble()
+  
+  if ("cnv_state" %in% column) {
+    
+    loh <- summaryTableExplain %>%
+      dplyr::filter(cnv_state == "LOH") %>%
+      nrow()
+    cause_final <- bind_rows(cause_final, tibble(cause = "LOH", num = loh))
+    
+    ASEunknown <- ASEunknown %>%
+      dplyr::filter(cnv_state != "LOH")
+    
+    summaryTableExplain$cnvRatioDiff = summaryTableExplain$majorAlleleFrequency - summaryTableExplain$expectedMAF
+    ASEunknown$cnvRatioDiff = ASEunknown$majorAlleleFrequency - ASEunknown$expectedMAF
+    
+    cnv <- summaryTableExplain %>%
+      dplyr::filter(cnv_state != "LOH") %>%
+      dplyr::filter(cnvRatioDiff< 0.10) %>%
+      nrow()
+    cause_final <- bind_rows(cause_final, tibble(cause = "CNV imbalance", num = cnv))
+    ASEunknown <- ASEunknown %>%
+      dplyr::filter(cnvRatioDiff <= 0.10)
+  }
+  
+  if ("somaticSNV" %in% column) {
+    somaticSNV <- summaryTableExplain %>%
+      dplyr::filter(somaticSNV) %>%
+      nrow()
+    cause_final <- bind_rows(cause_final, tibble(cause = "Somatic SNV", num = somaticSNV))
+    ASEunknown <- ASEunknown %>%
+      dplyr::filter(!somaticSNV)
+  }
+  if ("somaticIndel" %in% column) {
+    somaticIndel <- summaryTableExplain %>%
+      dplyr::filter(somaticIndel) %>%
+      nrow()
+    cause_final <- bind_rows(cause_final, tibble(cause = "Somatic Indel", num = somaticIndel))
+    ASEunknown <- ASEunknown %>%
+      dplyr::filter(!somaticIndel)
+  }
+  
+  if ("methyl_state" %in% column) {
+    methyl <- summaryTableExplain %>%
+      dplyr::filter((methyl_state < 0 & allele1IsMajor) | (methyl_state > 0 & !allele1IsMajor)) %>%
+      nrow()
+    cause_final <- bind_rows(cause_final, tibble(cause = "Allelic methylation", num = methyl))
+    ASEunknown <- ASEunknown %>%
+      dplyr::filter(is.na(methyl_state) | !((methyl_state < 0 & allele1IsMajor) | (methyl_state > 0 & !allele1IsMajor)))
+  }
+  
+  if ("stop_variant_allele" %in% column) {
+    stop <- summaryTableExplain %>% 
+      dplyr::filter((allele1IsMajor & stop_variant_allele == 2) | (!allele1IsMajor & stop_variant_allele == 1)) %>%
+      nrow()
+    cause_final <- bind_rows(cause_final, tibble(cause = "Stop variant", num = stop))
+    ASEunknown <- ASEunknown %>%
+      dplyr::filter(is.na(stop_variant_allele) | !((allele1IsMajor & stop_variant_allele == 2) | (!allele1IsMajor & stop_variant_allele == 1)))
+  }
+  if ("tf_allele" %in% column) {
+    tfbs <- summaryTableExplain %>%
+      dplyr::filter(!is.na(transcriptionFactor)) %>%
+      nrow()
+    cause_final <- bind_rows(cause_final, tibble(cause = "TFBS variant", num = tfbs))
+    ASEunknown <- ASEunknown %>%
+      dplyr::filter(is.na(transcriptionFactor))
+  }
+  
+  unknown <- nrow(ASEunknown)
+  cause_final <- bind_rows(cause_final, tibble(cause = "Unknown", num = unknown))
+
+  
+   return(cause_final)
+}
+
+################
+# ASE mechanism
+################
+aseMechanism <- causeFunction(summary_table)
+
+aseCause <- aseMechanism %>%
+  ggplot(aes(reorder(cause, -num), num)) + 
+  geom_bar(aes(fill = cause), stat = "identity") +
+  theme_bw() + 
+  scale_fill_npg() +
+  scale_color_npg() + 
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(), 
+        legend.position = "none") + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
+  xlab("ASE mechanism") + 
+  ylab("Gene frequency") + 
+  ggtitle("Frequency of all ASE genes for each genetic mechanism", 
+          subtitle = sample)
+
+ggsave(filename = paste0(out, "/aseCause.pdf"), plot = aseCause, units = "in")
+
+write.table(aseMechanism, file = paste0(out, "/tables/aseCause.tsv"),
+            quote = F, sep = "\t", row.names = F, col.names = T)
+
+################################
+# Cancer ASE mechanism
+################################
+cancerMechanism <- summary_table %>%
+  dplyr::filter(cancer_gene) %>%
+  causeFunction()
+
+
+cancerCause <- cancerMechanism %>%
+  ggplot(aes(reorder(cause, -num), num)) + 
+  geom_bar(aes(fill = cause), stat = "identity") +
+  theme_bw() + 
+  scale_fill_npg() +
+  scale_color_npg() + 
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(), 
+        legend.position = "none") + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
+  xlab("ASE mechanism") + 
+  ylab("Gene frequency") + 
+  ggtitle("Frequency of cancer ASE genes for each genetic mechanism", 
+          subtitle = sample)
 ggsave(filename = paste0(out, "/cancerCause.pdf"), plot = cancerCause, units = "in")
+
+
+write.table(cancerMechanism, file = paste0(out, "/tables/cancerCause.tsv"),
+            quote = F, sep = "\t", row.names = F, col.names = T)

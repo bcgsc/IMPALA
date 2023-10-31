6f5434781813cb047d56ce6bce43856ca39b2162
Glenn Chang <glchang@bcgsc.ca>
2023-10-31 11:04:48 -0700

    fix bug for importing expression matrix

--- src/compareTFBS.R
+++ src/compareTFBS.R
@@ -29,7 +29,7 @@ sample <- opt$sample
 
 ## GET EXPRESSED TF
 expressionMatrix <- read_delim(opt$expression_matrix, delim = "\t")
-transcriptionFactor <- read.delim(opt$tf, sep = "\t", header = T) %>%
+transcriptionFactor <- read_delim(opt$tf, delim = "\t") %>%
   separate(Transcription.factor, sep = ":", into=c("human", "transcriptionFactor")) %>%
   dplyr::select(-human)
 
